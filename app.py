f408d6018fa7ab443a9e20c403d0c2dfd727230d
Brunno Soares <brunnox@eoutlook.com>
2024-09-08 14:59:12 -0300

    Final version

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, url_for
 import qrcode
 from io import BytesIO
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    qr_code_url = request.args.get('qr_code_url')
+    return render_template('index.html', qr_code_url=qr_code_url)
 
 @app.route('/generate_qr')
 def generate_qr():
@@ -18,13 +20,24 @@ def generate_qr():
     img_io = BytesIO()
     qr.save(img_io, 'PNG')
     img_io.seek(0)
+
+    tmp_file_path = os.path.join('/tmp', 'qrcode.png')
     
-    qr_code_url = '/static/qrcode.png'
-    
-    with open('static/qrcode.png', 'wb') as f:
+    with open(tmp_file_path, 'wb') as f:
         f.write(img_io.getvalue())
-    
+
+    qr_code_url = url_for('serve_qr_code', _external=True)
     return render_template('index.html', qr_code_url=qr_code_url)
+
+@app.route('/qr_code')
+def serve_qr_code():
+    tmp_file_path = os.path.join('/tmp', 'qrcode.png')
+    return send_file(tmp_file_path, mimetype='image/png')
+
+@app.route('/download_qr')
+def download_qr_code():
+    tmp_file_path = os.path.join('/tmp', 'qrcode.png')
+    return send_file(tmp_file_path, as_attachment=True, download_name='qrcode.png', mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
